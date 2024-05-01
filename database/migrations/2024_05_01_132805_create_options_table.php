8a9e00cb8e3465429de4fdd0a64caf3184da2f40
razabangi <muhammadrazabangi9@gmail.com>
2024-05-01 18:34:08 +0500

    create schema

---
+++ database/migrations/2024_05_01_132805_create_options_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Poll;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private $tableName = 'options';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Poll::class)->constrained();
+            $table->string('title');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+};
