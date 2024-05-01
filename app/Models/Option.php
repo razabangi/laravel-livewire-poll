670168e62f14862730687b3b4ec7b886d7a51a94
razabangi <muhammadrazabangi9@gmail.com>
2024-05-01 18:50:45 +0500

    setup models

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Option extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['poll_id', 'title'];
+
+    public function poll(): BelongsTo
+    {
+        return $this->belongsTo(Poll::class);
+    }
 }
